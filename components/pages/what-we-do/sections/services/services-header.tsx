312af18a9247372e2c83d5194b0bb72e1463ab16
Richard Kaitare <rkaitare@gmail.com>
2024-12-19 16:47:05 +0200

    Delete components/pages/what-we-do directory
    it has a lot of errors

--- components/pages/what-we-do/sections/services/services-header.tsx
+++
@@ -1,7 +0,0 @@
-export function ServicesHeader() {
-  return (
-    <h2 className="mb-12 text-3xl font-bold tracking-tight lg:text-4xl">
-      OUR BUILDING CONSTRUCTION OFFER
-    </h2>
-  )
-}
--- components/pages/what-we-do/sections/stats/stats-header.tsx
+++
@@ -1,7 +0,0 @@
-export function StatsHeader() {
-  return (
-    <h2 className="text-center text-3xl font-bold text-white">
-      SMC2 IN NUMBERS
-    </h2>
-  )
-}
